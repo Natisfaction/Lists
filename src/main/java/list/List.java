774405fa095b34383e40cd5f3bc4fe361564ac9d
Natisfaction <cl406761@gmail.com>
2023-10-05 21:40:27 +0200

    Better refactoring, implemented reverse

--- src/main/java/list/List.java
+++ src/main/java/list/List.java
@@ -28,16 +28,26 @@ public class List {
         return sublist.length();
     }
 
-    public void sort() {
-        //
+    public void reverse() {
+        for(int i = 0; i < length() / 2; i++)
+            exchange(i, length() - 1 - i);
     }
 
-    public void reverse() {
-        //
+    public boolean exchange(int source, int destination) {
+        if(source < 0 || destination < 0 || source > length() || destination > length())
+            return false;
+
+        // Required for objects
+        Object sour = getElement(source);
+        Object dest = getElement(destination);
+
+        replace(sour, destination);
+        replace(dest, source);
+
+        return true;
     }
 
     // Get the element at the index (INDEX STARTS WITH 0)
-
     // - If the function fails, null is returned
     // - If the function succeeds, the requested element is returned
     public Object getElement(int index) {
@@ -51,15 +61,15 @@ public class List {
     }
 
     // Remove the latest element
-    public int remove() {
+    public boolean remove() {
         return remove(length() - 1);
     }
 
     // Removes a specific element
-    public int remove(int index) {
+    public boolean remove(int index) {
         // Check if the index is greater than the length of the list
         if(index > length() || index < 0)
-            return -1;
+            return false;
 
         // Check if the desired element is the current
         if(this.index != index)
@@ -69,7 +79,7 @@ public class List {
         // 1) Copy the next element
         if(sublist == null) {
             element = null;
-            return 0;
+            return true;
         }
 
         element = sublist.element;
@@ -79,11 +89,37 @@ public class List {
             element = null;
             sublist = null;
 
-            return 0;
+            return true;
         }
 
         // 3) Else, just repeat till you find the last element
         return remove(index + 1);
+    }
+
+    /* Replaces the element, at the position */
+    public boolean replace(Object element, int position) {
+        if(position < 0 || position > length())
+            return false;
+
+        if(position != this.index) {
+            return sublist.replace(element, position);
+        }
+
+        this.element = element;
+        return true;
+    }
+
+    // Add function
+    public void add(Object... elements) {
+        for(Object singleElement: elements) {
+            if (sublist != null) {
+                sublist.add(singleElement);
+            }
+            else {
+                this.element = singleElement;
+                sublist = new List(index);
+            }
+        }
     }
 
     public String toString() {
@@ -117,17 +153,5 @@ public class List {
         ret += sublist.toString();
 
         return ret;
-    }
-
-    public void add(Object... elements) {
-        for(Object singleElement: elements) {
-            if (sublist != null) {
-                sublist.add(singleElement);
-            }
-            else {
-                this.element = singleElement;
-                sublist = new List(index);
-            }
-        }
     }
 }
--- src/main/java/list/Main.java
+++ src/main/java/list/Main.java
@@ -1,17 +1,17 @@
 package list;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         List l1 = new List();
 
-        for(int i = 0; i < 6; i++) {
+        for(int i = 1; i < 9; i++) {
             l1.add(i);
         }
 
         List l2 = new List(l1);
         l1.add(l2);
+
+        l1.reverse();
 
         System.out.print(l1.toString()+"\n"+l1.length()+"\n");
     }
